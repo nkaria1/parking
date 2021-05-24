442c08f97126ab212489bef947c7b1302e07f8b3
Niharika Karia <niharika.karia@aspentech.com>
2021-05-24 14:45:06 -0400

    fix for lint formatting

--- Code/preprocess.py
+++ Code/preprocess.py
@@ -3,11 +3,11 @@ import numpy as np
 
 img = cv2.imread("Data/detect_blob.png")
 print(img.shape)
-img_resize = cv2.resize(img, (200, 200 ) )  #x,y
+img_resize = cv2.resize(img, (200, 200))  # x,y
 
-imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV )
+imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 hz = np.hstack((img, imgHsv))
-cv2.imshow("hsv", hz )
+cv2.imshow("hsv", hz)
 cv2.waitKey(5000)
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
