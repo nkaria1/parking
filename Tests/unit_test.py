a79fe800cf5d413ca5ca3da42b533f1ba8e9d603
Niharika Karia <niharika.karia@aspentech.com>
2021-05-24 15:28:31 -0400

    check curren working directory

--- Tests/unit_test.py
+++ Tests/unit_test.py
@@ -1,8 +1,14 @@
 import unittest
 import cv2
+import os
 
 class CarParkingTestCase(unittest.TestCase):
     def test_shape(self):
+        # Get the current working directory
+        cwd = os.getcwd()
+
+        # Print the current working directory
+        print("Current working directory: {0}".format(cwd))
         input = cv2.imread("../Data/detect_blob.png")
         expected_output = (760,541,3)
         result = input.shape
